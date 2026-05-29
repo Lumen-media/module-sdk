8662a89c8010f2d0ae82b993d1f4bc12619707e1
gabrielSantos1101 <gabriel_santos.dev@outlook.com.br>
2026-05-29 15:02:23 -0300

    fix(module-cli): bundle internal deps instead of externalizing them

--- packages/module-cli/tsup.config.ts
+++ packages/module-cli/tsup.config.ts
@@ -8,9 +8,6 @@ export default defineConfig({
 	clean: true,
 	banner: { js: "#!/usr/bin/env node" },
 	external: [
-		"@lumen-media/create-module",
-		"@lumen-media/module-build",
-		"@lumen-media/module-sdk",
 		"archiver",
 		"vite",
 	],
